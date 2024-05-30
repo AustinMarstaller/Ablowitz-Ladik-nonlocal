e44f4959055b8b91c80cafd0ebfcb0f8e937cc3e
Austin Marstaller <amarstaller@smu.edu>
2024-05-30 12:07:01 -0500

    MI evolution performing correctly.

--- AL_NONLOCAL_ODE23.m
+++ AL_NONLOCAL_ODE23.m
@@ -9,9 +9,9 @@ x=[-L/2:dx:L/2-dx]'; % Grid
 [row col] = size(x);
 %% Parameters
 alpha = 1; % Fixed value of alpha used in calculating A
-A     = 0.3; % Value of A to allow gain to be nonzero
-kappa = 0.0905602; % Fixed value of kappa used in calculating A
-delta = 0.001;
+A     = 0.25; % Value of A to allow gain to be nonzero
+kappa = 0.0653585; % Fixed value of kappa used in calculating A
+delta = 0.0001;
 
 initial_condition = A + delta*exp(1i*kappa*x);
 
@@ -19,14 +19,16 @@ initial_condition = A + delta*exp(1i*kappa*x);
 LRI_MATRIX = AL_PERIODIC(L/2,alpha); 
 
 % Define the problem: d/dt u_n = -i * ( 1 + |u_n|^2 ) * L_alpha u_n) 
-RHS = @(t,v) -1i*( LRI_MATRIX*v + abs(v).^2 .* LRI_MATRIX * v );
+RHS = @(t,v) -1i*( LRI_MATRIX*v + abs(v).^2 .* (LRI_MATRIX * v) );
 
-[tt,uu] = ode23(RHS, [0,30],initial_condition); % Solve problem
+[tt,uu] = ode45(RHS, [0,100],initial_condition); % Solve problem
 
 % Solution plotting
 figure 
-imagesc(tt,x,abs(uu-A)'.^2); 
-%pcolor(tt,x,abs((uu-A)').^2);
+
+%imagesc(tt,x,abs(uu-A)'.^2); 
+pcolor(tt,x,abs((uu-A)').^2);
+cmocean('balance')
 colorbar; 
 shading interp;
 xlabel("TIME");
@@ -34,23 +36,26 @@ ylabel("Grid"),
 
 title('Initial condition: $A + \delta e^{i\kappa n}$','Interpreter','latex','FontSize',16)
 subtitle("\kappa="+kappa+", A="+A+", \alpha="+alpha+", \delta="+delta)
-%exportgraphics(gcf,'MI_attempt_kappa_0956.pdf','ContentType','vector');
+exportgraphics(gcf,'MI_attempt.pdf','ContentType','vector');
 
 
-%% Compute and plot PSD
+%% Compute and plot spatial FT at final time
 figure
 n=length(x);
-uhat = fft(uu-A, n,2);
+uhat = fft(uu(end,:)-A, n,2);
 PSD =  uhat.*conj(uhat)/n;
-%freq = 1/(0.001*n)*(0:n-1);
 freq = (2*pi/n)*[-L/2:L/2-1];
 plot(freq,fftshift(PSD), 'LineWidth',2.5)
 set(gca,'FontSize',16)
 xlabel('Frequency')
 ylabel('Spatial FT')
-title('Power Spectral Density: $|\hat{u}|^2$','Interpreter','latex','FontSize',16)
-%exportgraphics(gcf,'PSD_attempt_kappa_0956.pdf','ContentType','vector');
+title('Spatial FT at final time: $|\hat{u_{tf}}|^2$','Interpreter','latex','FontSize',16)
+exportgraphics(gcf,'SpatialFT_attempt.pdf','ContentType','vector');
+%Dormand-Prince integratorexportgraphics(f,'bartransparent.pdf','ContentType','vector',...
+%               'BackgroundColor','none')
 sprintf("Simulation completed in: %0.5f", toc)
+
+
 % Modified Long range interaction on the lattice with periodic BC on n in {-N, ..., N-1}
 % See Appendix for the exact expression of periodic operator
 function M = AL_PERIODIC(N,a)
@@ -76,14 +81,7 @@ for r = 1:numel(r_INDEX)
     
     % Compute the coefficient values
     znr =  (hurwitzZeta(1+a, r/(2*N)));
-    
-    % Don't need this anymore, since we won't be overwriting the same entry
-    % in `data`. We'll be incrementing the same index in M_DENSE_firstRow
-    % twice.
-%     if congruence_sum == congruence_difference
-%         znr = 2*znr;
-%     end
-    
+       
     znr = 1/(2*N)^(1+a) * znr;
     
     % Increment M_DENSE_firstRow on the corresponding columns
